98243f1619d1cb2610f771245c482290658ca2fa
Prajjwal Majumder <majumderprajjwal@gmail.com>
2026-04-29 00:44:08 +0530

    Added test.py for testing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GITdemo
 This repository is just a demo to get much more familiar with git and github
+<br>
+This is a new line which needs to be added.
---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 00:14:35 2024
+
+@author: prajjwal
+"""
+
+print("Hey there!")
+
+print("Move aside!")
+
+print("This is the first change!")
