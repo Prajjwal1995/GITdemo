5dc63009cbc6d354fbc667315c18fe33927e06db
Prajjwal Majumder <majumderprajjwal@gmail.com>
2026-05-10 00:47:02 +0530

    Changed test.py in spyder

--- test.py
+++ test.py
@@ -17,3 +17,8 @@ print("This is second change")
 print("This is the line added in dev1")
 
 print("Hello from dev1 branch. I am codex")
+
+a = float(input("Enter a:"))
+b = float(input("Enter b:"))
+c = a+b
+print("c is",c)
